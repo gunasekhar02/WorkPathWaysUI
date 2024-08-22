1f30fd4cb7b55ad17db6ccdc08fb9a3ad84eba88
Guna Sekhar Reddy Peddireddygari <101317857+gunasekhar02@users.noreply.github.com>
2024-08-22 09:58:42 +0530

    Added tooltips and the back buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatTooltipModule} from '@angular/material/tooltip';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatToolbarModule,MatButtonModule,MatIconModule,MatCardModule,RouterLink],
+  imports: [RouterOutlet,MatToolbarModule,MatButtonModule,MatIconModule,MatCardModule,RouterLink,MatTooltipModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
